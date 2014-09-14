090c7198098c370e28427ddf6b5049a9e28ffcf9
EngineeringIsLife <daschfg@googlemail.com>
2014-09-14 14:30:58 +0200

    Refactoring

--- ElectronicStation/Brackets.py
+++ ElectronicStation/Brackets.py
@@ -6,18 +6,20 @@ Created on Sun Sep 14 13:27:33 2014
 """
 
 def checkio(expression):
-    symbols = {'{':'}', '[':']', '(':')'}
-    brackets = []
+    brackets = {'{':'}', '[':']', '(':')'}
+    opened = []
     for c in expression:
-        if c in symbols.keys():         # Open bracket
-            brackets.append(c)        
-        elif c in symbols.values():     # Try to close bracket
-            if len(brackets) and c is symbols[brackets[-1]]: # Bracket can be closed
-                brackets.pop()
+        if c in brackets.keys():        # Open bracket
+            opened.append(c)        
+        elif c in brackets.values():    # Try to close bracket
+            if not len(opened):         # Too much close brackets
+                return False
+            elif c is brackets[opened[-1]]: # Bracket can be closed
+                opened.pop()
             else:                       # Bracket cannot be closed
                 return False
-    return not len(brackets)            # Unclosed brackets left?
-            
+    return not len(opened)              # Any unclosed brackets left?
+
 
 print checkio("((5+3)*2+1)") == True
 print checkio("{[(3+1)+2]+}") == True
