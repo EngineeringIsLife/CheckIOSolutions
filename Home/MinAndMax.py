c0fb906e060c126eeda01f43deae8d3f2ee36bab
EngineeringIsLife <daschfg@googlemail.com>
2014-09-17 19:55:25 +0200

    New solution
    First attempt to solve Min and Max
    Works, but lot of room for improvement

---
+++ Home/MinAndMax.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 17 17:57:12 2014
+
+@author: EngineeringIsLife
+"""
+
+from types import GeneratorType
+
+def min(*args, **fcn_in):
+    lastlevel = args[0] if len(args) is 1 else args
+    if isinstance(lastlevel, set):
+        lastlevel = list(lastlevel)
+        best = lastlevel[0]
+    elif isinstance(lastlevel, GeneratorType):
+        best = lastlevel.next()
+    else:
+        best = lastlevel[0]
+    
+    fcn = fcn_in["key"] if fcn_in else lambda x: x
+    for arg in lastlevel:
+        if fcn(arg) < fcn(best):
+            best = arg
+    return best
+    
+def max(*args, **fcn_in):
+    lastlevel = args[0] if len(args) is 1 else args
+    if isinstance(lastlevel, set):
+        lastlevel = list(lastlevel)
+        best = lastlevel[0]
+    elif isinstance(lastlevel, GeneratorType):
+        best = lastlevel.next()
+    else:
+        best = lastlevel[0]
+    fcn = fcn_in["key"] if fcn_in else lambda x: x
+    for arg in lastlevel:
+        if fcn(arg) > fcn(best):
+            best = arg
+    return best
+
+#max(3, 2) == 3
+print min({1, 2, 3, 4, -10})
+print min(abs(i) for i in range(-10, 10))
+print max(abs(i) for i in range(-10, 10))
+print min(3, 2) == 2
+print min(2, 3) == 2
+print min([1, 2, 0, 3, 4]) == 0
+print max([1, 2, 0, 3, 4]) == 4
+print min("hello") == "e"
+print min(3.2,2,key=int) == 2
+print min(2.2, 5.6, 5.9,2.1, key=int) == 2.2# == 5.6
+print max(2.2, 5.6, 5.9, key=int) == 5.6
+print min([[1,2], [3, 4], [9, 0]], key=lambda x: x[1]) == [9, 0]
