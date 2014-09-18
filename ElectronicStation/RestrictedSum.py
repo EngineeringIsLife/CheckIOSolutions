79da4eec2f982c599932d83b70e7d6f074c77616
EngineeringIsLife <daschfg@googlemail.com>
2014-09-18 13:20:20 +0200

    Refactoring
    Shorter solution, similar way

--- ElectronicStation/RestrictedSum.py
+++ ElectronicStation/RestrictedSum.py
@@ -5,14 +5,9 @@ Created on Thu Sep 18 13:00:28 2014
 @author: EngineeringIsLife
 """
 
-def get(currentList, currentVal):
-    currentVal += currentList.pop()
-    if currentList:
-        currentVal = get(currentList, currentVal)
-    return currentVal
 
 def checkio(data):
-    return get(data, 0)
+    return data[0]+checkio(data[1:]) if len(data) > 1 else data[0]
 
 print checkio([1, 2, 3])# == 6
 print checkio([2, 2, 2, 2, 2, 2])# == 12
